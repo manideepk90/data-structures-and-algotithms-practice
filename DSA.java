f88d28f71026abd449cc317e91c86db0c3be1e79
KUNTIMADDI MANIDEEP <manideepk90@gmail.com>
2024-07-18 08:11:15 +0530

    update

--- DSA.java
+++ DSA.java
@@ -1,29 +1,26 @@
 
-import linkedlist.LinkedList;
-import linkedlist.Node;
-
 public class DSA {
 
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.append(10);
-        list.append(20);
-        Node node = new Node(2);
-        list.append(node);
-        list.append(16);
-        list.push(15);
-        list.append(23);
-        list.print();
-        System.out.println("");
+        // LinkedList list = new LinkedList();
+        // list.append(10);
+        // list.append(20);
+        // Node node = new Node(2);
+        // list.append(node);
+        // list.append(16);
+        // list.push(15);
+        // list.append(23);
+        // list.print();
+        // System.out.println("");
         // list.append(58);
-        // // list.insert(node, 55);
-        // // list.deleteFirst();
-        // list.deleteLast();        list.deleteLast();
+        // list.insert(node, 55);
+        // list.deleteFirst();
         // list.deleteLast();
-        list.insert(5,3);
+        // list.deleteLast();
+        // list.deleteLast();
+        // list.insert(5, 3);
         // list.delete(1);
-        list.print();
-   
-     
+        // list.print();
+
     }
 }
